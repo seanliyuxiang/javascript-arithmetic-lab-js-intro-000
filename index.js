0e7462918d24076bb2d084a3a6a7515d4d4483da
Yuxiang (Sean) Li <seanliyuxiang@gmail.com>
2020-09-19 06:12:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ function decrement(n) {
   return n-=1;
 }
 
-function makeInt(n) {
-  return parseInt(n,10)
+function makeInt(str) {
+  return parseInt(str,10)
 }
+
+// function preserveDecimal(n) {
+//   return parseFloat()
+// }
